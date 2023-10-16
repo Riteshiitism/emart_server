3bea2243fe013d1cdc03f2267f0b0a3473272d76
Riteshiitism <120582841+Riteshiitism@users.noreply.github.com>
2023-10-16 19:54:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -14,10 +14,23 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-  origin: ["http://localhost:3000","https://cool-dolphin-debfaf.netlify.app/"],
-  credentials: true
-}));
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ['http://localhost:3000', 'https://cool-dolphin-debfaf.netlify.app'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
+
+// app.use(cors({
+//   origin: ["http://localhost:3000","https://cool-dolphin-debfaf.netlify.app/"],
+//   credentials: true
+// }));
 // cors: {
 //   origin: ["http://localhost:3000", "https://cool-dolphin-debfaf.netlify.app/"];
 // }
