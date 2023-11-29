db577daded42d2f32464f6dbf4c4d1a633a66ef9
Riteshiitism <120582841+Riteshiitism@users.noreply.github.com>
2023-11-29 18:20:14 +0530

    Update tags.js

--- controllers/tags.js
+++ controllers/tags.js
@@ -1,13 +1,31 @@
-const tags = require('./../models/tagsModel')
+const Product = require('./../models/productModel')
 
 const findBytag = async (name)=>{
-    const obj = await tags.findOne({"name":name})
+    // console.log(name);
+    // const obj = await Product.find({tagsUI: {$elemMatch:{}}})
+    // const obj = await Product.find();
+
+    const obj = await Product.find({
+        tagsUI: { $all: name }
+      });
+
+
+    // find({awards: {$elemMatch: {award:'National Medal', year:1975}}})
+
+
+    // console.log(obj);
+
+
+    // db.collection.find({
+    //     "entries.name": "frank",
+    //     "entries.email": "daniel@email.com"
+    //   })
     // console.log(obj, typeof obj)
     if(obj == null){
         return([])
     }
     else{
-    return((obj.items))}
+    return((obj))}
 }
 
 module.exports ={
